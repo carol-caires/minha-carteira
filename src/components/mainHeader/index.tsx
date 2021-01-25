d2e2ea025eb00ce526ae6deccb5f1405c4951adb
Carol Caires <carolbazarello@gmail.com>
2021-01-25 17:47:22 -0300

    create welcome message in header

--- src/components/mainHeader/index.tsx
+++ src/components/mainHeader/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { Container } from './styles'
+import React, { useMemo}  from 'react'
+import { Container, Profile, Welcome, UserName } from './styles'
+import emojis from '../../utils/emojis'
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const i = Math.floor(Math.random() * emojis.length);
+        return emojis[i]
+    }, [])
+
     return (
         <Container>
-            <h1>MainHeader</h1>
+            <h1>Toggle</h1>
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName>Carol Caires</UserName>
+            </Profile>
         </Container>
     );
 }
--- src/components/mainHeader/styles.ts
+++ src/components/mainHeader/styles.ts
@@ -5,4 +5,17 @@ export const Container = styled.div`
 
     color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.secondary};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+    border-bottom: 1px solid ${props => props.theme.colors.gray};
+
 `
+
+export const Profile = styled.div`
+    color: ${props => props.theme.colors.white}
+
+`
+export const Welcome = styled.h3``
+export const UserName = styled.span``
