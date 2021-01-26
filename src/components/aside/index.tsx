8c4c6e90bee63cc6f00096196d4b068377e85322
Carol Caires <carolbazarello@gmail.com>
2021-01-26 08:50:00 -0300

    create sidebar with header and menu

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import { Container } from './styles'
+import { Container, Header, LogoImg, Title, MenuContainer, MenuItemLink } from './styles'
+
+import LogoFile from '../../assets/logo.svg'
+
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp
+} from 'react-icons/md'
 
 const Aside: React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
+           <Header>
+                <LogoImg src={LogoFile} alt="Logo Minha Carteira"/>
+                <Title>Minha Carteira</Title>
+           </Header>
+
+           <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard/> 
+                    Dashboard
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowDownward/> 
+                    Entradas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowUpward/> 
+                    Saídas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdExitToApp/> 
+                    Sair
+                </MenuItemLink>
+           </MenuContainer>
         </Container>
     );
 }
--- src/components/aside/styles.ts
+++ src/components/aside/styles.ts
@@ -5,4 +5,47 @@ export const Container = styled.div`
 
     color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.secondary};
+
+    padding-left: 20px;
+    border-right: 1px solid ${props => props.theme.colors.gray};
+`
+
+export const Header = styled.header`
+    display: flex;
+    height: 70px;
+    align-items: center;
+`
+
+export const LogoImg = styled.img`
+    height: 40px;
+    width: 40px;
+`
+
+export const Title = styled.h3`
+    color: ${props => props.theme.colors.white};
+    margin-left: 10px;
+`
+
+export const MenuContainer = styled.nav`
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+`
+
+export const MenuItemLink = styled.a`
+    color: ${props => props.theme.colors.info};
+    text-decoration: none;
+    margin: 7px 0;
+    display: flex;
+    align-items: center;
+
+    transition: opacity .3s;
+    &:hover{
+        opacity: .7;
+    }
+
+    > svg {
+        font-size: 18px;
+        margin-right: 5px;
+    }
 `
